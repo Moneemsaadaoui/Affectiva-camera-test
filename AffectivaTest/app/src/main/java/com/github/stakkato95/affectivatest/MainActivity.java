203ef8fcf1888c7ca3c8b40a26be6adfe4c40cf1
Artsiom Kaliaha <stakkato95@gmail.com>
2017-09-07 11:40:44 +0300

    added camera detection with appropriate aspect ratio

--- AffectivaTest/app/src/main/java/com/github/stakkato95/affectivatest/MainActivity.java
+++ AffectivaTest/app/src/main/java/com/github/stakkato95/affectivatest/MainActivity.java
@@ -1,13 +1,116 @@
 package com.github.stakkato95.affectivatest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceView;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.affectiva.android.affdex.sdk.Frame;
+import com.affectiva.android.affdex.sdk.detector.CameraDetector;
+import com.affectiva.android.affdex.sdk.detector.Detector;
+import com.affectiva.android.affdex.sdk.detector.Face;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    CameraDetector cameraDetector;
+    boolean isPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SurfaceView surfaceView = findViewById(R.id.surface);
+        cameraDetector = new CameraDetector(
+                this,
+                CameraDetector.CameraType.CAMERA_FRONT,
+                surfaceView
+        );
+
+        cameraDetector.setImageListener(new Detector.ImageListener() {
+            @Override
+            public void onImageResults(List<Face> list, Frame frame, float v) {
+                if ((list != null) && !list.isEmpty()) {
+                    Toast.makeText(
+                            MainActivity.this,
+                            "joy=" + list.get(0).emotions.getJoy(),
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        });
+
+        cameraDetector.setOnCameraEventListener(new CameraDetector.CameraEventListener() {
+            @Override
+            public void onCameraSizeSelected(final int cameraHeight, final int cameraWidth, Frame.ROTATE rotation) {
+                surfaceView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        int layoutWidth = surfaceView.getWidth();
+                        int layoutHeight = surfaceView.getHeight();
+
+                        float layoutAspectRatio = (float) layoutWidth / layoutHeight;
+                        float cameraAspectRatio = (float) cameraWidth / cameraHeight;
+
+                        int newWidth;
+                        int newHeight;
+
+                        if (cameraAspectRatio > layoutAspectRatio) {
+                            newWidth = (int) (layoutHeight * cameraAspectRatio);
+                            newHeight = layoutHeight;
+                        } else {
+                            newWidth = layoutWidth;
+                            newHeight = (int) (layoutWidth / cameraAspectRatio);
+                        }
+
+                        ViewGroup.LayoutParams params = surfaceView.getLayoutParams();
+                        params.height = newHeight;
+                        params.width = newWidth;
+                        surfaceView.setLayoutParams(params);
+                    }
+                });
+            }
+        });
+
+
+        int permissionCheckResult = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+            cameraDetector.start();
+            isPermissionGranted = true;
+            return;
+        }
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (isPermissionGranted) {
+            cameraDetector.start();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (isPermissionGranted) {
+            cameraDetector.stop();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            isPermissionGranted = true;
+            cameraDetector.start();
+        }
     }
 }
